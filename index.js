f500c4f9c9b1ac4a1741ac519d0174da0f32a20b
brunosampaio222 <brunosampaio2223@gmail.com>
2023-11-19 16:50:40 -0300

    Inserindo dados no banco | Node e MySQL

--- index.js
+++ index.js
@@ -11,18 +11,59 @@ app.set("view engine", "handlebars")
 // Pasta de Arquivos estaticos como CSS, imagens
 app.use(express.static("public"))
 
-// Trabalhar com dados no formar Json
-
+// Trabalhar com dados no formato Json
 app.use(express.urlencoded({
     extended: true
 }))
 
+// CRUD => (  Create, Read, Update, Delete  )
+
 app.use(express.json())
 
+
 //Rotas
-app.get("/", (requisicao, resposta) => {
-    resposta.render("home")
+app.post("/register/save", (request, response) => {
+    const { title, pageqty } = request.body
+
+    const book = 
+    {
+        title: title,
+        pageqty: pageqty
+    }
+
+    const query = `
+        INSERT INTO books (title, pageqty)
+        VALUES ('${book.title}', '${book.pageqty}')
+    `
+
+    conn.query(query, (error) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        response.redirect("/")
+    })
 })
+
+app.get("/register", (request, response) => {
+    response.render("register")
+})
+
+app.get("/", (resquest, response) => {
+    const sql = 'SELECT * FROM books'
+
+    conn.query(sql, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        const books = data
+
+        response.render("home", { books })
+    })
+})
+
 
 //Conexão com MySQL
 const conn = mysql.createConnection({
